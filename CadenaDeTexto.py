f23ecf270f1baca31d7950b091dd96f4c9f89c46
nilander1 <102563137+nilander1@users.noreply.github.com>
2026-04-14 21:12:33 -0300

    first commit

---
+++ CadenaDeTexto.py
@@ -0,0 +1,6 @@
+Texto=  'te quiero solo como amigo'
+print(Texto[:2])
+print(Texto[-3:])
+print(Texto[::2])
+print(Texto[::-1])
+print(Texto+Texto[::-1])
---
+++ EntradaYSalidaDeDatos.py
@@ -0,0 +1,2 @@
+Nombre=input('¿Cual es tu nombre? =')
+Edad=int(input('¿Cual es tu edad? ='))
