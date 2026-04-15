3b18d43d8600c028b2806ddc265c02bb96e57219
nilander1 <102563137+nilander1@users.noreply.github.com>
2026-04-14 21:16:20 -0300

    Delete EcuacionDeSegundoGrado.py

--- EcuacionDeSegundoGrado.py
+++
@@ -1,9 +0,0 @@
-av=float(input('insertar valor de a= '))
-bv=float(input('insertar valor de b= '))
-cv=float(input('insertar valor de c= '))
-EcuacionP= -bv+((2*av/(bv**2)-4*av-cv))**1/2
-EcuacionN= -bv-((2*av/(bv**2)-4*av-cv))**1/2
-
-print(EcuacionP)
-print(EcuacionN)
-
